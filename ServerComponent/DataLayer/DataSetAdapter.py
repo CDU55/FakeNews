6cfcee60adcf83209e12539a0be41d0754c18ee9
CDU55 <48677296+CDU55@users.noreply.github.com>
2020-11-16 23:40:44 +0200

    Social Media Posts Handler

--- ServerComponent/DataLayer/DataSetAdapter.py
+++ ServerComponent/DataLayer/DataSetAdapter.py
@@ -1,10 +1,19 @@
 from DataLayer import DataSetProvider
 from DataLayer.DataSetEntry import SocialMediaDataSetEntry
+import numpy as np
+from Analysers.NaiveBayes import NaiveBayes
 
 
 class DataSetAdapter:
     def __init__(self):
         self.data_set_provider = DataSetProvider.DataSetProvider()
+        self.notations = {
+            "followers_number": {"Very Low": 1, "Low": 2, "Medium": 3, "High": 4, "Very High": 5},
+            "likes_number": {"Very Low": 1, "Low": 2, "Medium": 3, "High": 4, "Very High": 5},
+            "comments_number": {"Very Low": 1, "Low": 2, "Medium": 3, "High": 4, "Very High": 5},
+            "spelling": {"Bad": 1, "Medium": 2, "Good": 3},
+            "length": {"Very short": 1, "Short": 2, "Medium": 3, "Long": 4, "Very Long": 5}
+        }
 
     def provide_dataset_entries(self):
         raw_data = self.data_set_provider.get_data_set_entries()
@@ -13,4 +22,28 @@ class DataSetAdapter:
             current_entry = SocialMediaDataSetEntry(entry[1], entry[2], entry[3], entry[4], entry[5], entry[6])
             entries.append(current_entry)
         return entries
+
+    def convert_to_training_datasets(self):
+        db_entries = self.provide_dataset_entries()
+        characteristics = np.zeros((len(db_entries), len(self.notations)))
+        labels = np.zeros(len(db_entries))
+        for index, post in enumerate(db_entries):
+            characteristics[index, 0] = self.notations["followers_number"][post.followers_number]
+            characteristics[index, 1] = self.notations["likes_number"][post.likes_number]
+            characteristics[index, 2] = self.notations["comments_number"][post.comments_number]
+            characteristics[index, 3] = self.notations["spelling"][post.spelling]
+            characteristics[index, 4] = self.notations["length"][post.length]
+            labels[index] = post.label
+        return characteristics, labels
+
+    def convert_to_predict_data(self, data: SocialMediaDataSetEntry):
+        predict_data = np.zeros(len(self.notations))
+        predict_data[0] = self.notations["followers_number"][data.followers_number]
+        predict_data[1] = self.notations["likes_number"][data.likes_number]
+        predict_data[2] = self.notations["comments_number"][data.comments_number]
+        predict_data[3] = self.notations["spelling"][data.spelling]
+        predict_data[4] = self.notations["length"][data.length]
+        return predict_data
+
+
 
--- ServerComponent/DataLayer/DataSetEntry.py
+++ ServerComponent/DataLayer/DataSetEntry.py
@@ -1,8 +1,8 @@
 class SocialMediaDataSetEntry:
-    def __init__(self, followers_number, likes_number, comments_number,spelling,length,label):
+    def __init__(self, followers_number, likes_number, comments_number, spelling, length, label):
         self.followers_number = followers_number
         self.likes_number = likes_number
         self.comments_number = comments_number
-        self.spelling=spelling
-        self.length=length
+        self.spelling = spelling
+        self.length = length
         self.label = label
