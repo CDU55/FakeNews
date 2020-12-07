a86c0eab6db58f6f5602eb7eba771fea3e622e01
CDU55 <48677296+CDU55@users.noreply.github.com>
2020-12-07 19:10:03 +0200

    Calculate text similarity

---
+++ ServerComponent/Analysers/SubjectRelevance.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import jellyfish
+
+
+def calculate_maximum_similarity(social_media_post_text):
+    chunksize = 10 ** 3
+    max_sim = 0
+    max_headline = ""
+    for chunk in pd.read_csv("india-news-headlines.csv", chunksize=chunksize):
+        for values_group in chunk.values:
+            current_headline = values_group[2]
+            current_sim = jellyfish.jaro_distance(social_media_post_text,current_headline)
+            if current_sim > max_sim:
+                max_sim = current_sim
+                max_headline = current_headline
+    print(max_sim)
+    print(max_headline)
+
+
+def extract_headlines_from_csv():
+    chunksize = 10 ** 3
+    file = open("headlines.txt", "w")
+    for chunk in pd.read_csv("india-news-headlines.csv", chunksize=chunksize):
+        for values_group in chunk.values:
+            file.write(values_group[2] + "\n")
