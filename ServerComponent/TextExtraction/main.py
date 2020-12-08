f02764879e121a4737a4a157f9ffe257580d2949
BOBIRE <mraboby@gmail.com>
2020-12-08 10:09:40 +0200

    Fix main.py from DataExtraction

--- ServerComponent/TextExtraction/main.py
+++ ServerComponent/TextExtraction/main.py
@@ -2,7 +2,7 @@ import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
-from APIs import WebAPI, TwitterAPI
+from APIs import WebAPI, TwitterAPI, TextAPI
 
 protv = 'https://incont.stirileprotv.ro/joburi-romania/cum-se-va-desfasura-telemunca-si-munca-la-domiciliu-si-cum-se-schimba-programul-de-lucru-noi-reglementari.html?_ga=2.18512444.441552134.1604862638-837112706.1604862638'
 fblink = 'https://www.facebook.com/PresedinteleTraianBasescu/posts/3918068968206625'
@@ -10,14 +10,13 @@ twitter = "https://mobile.twitter.com/pinkfloyd/status/1325772237341945858"
 badea = "https://mobile.twitter.com/lupulalbastru1/status/1284161708194504705"
 test = "https://mobile.twitter.com/RockstarGames/status/1327306496217128967"
 
-
 ### FOR NORMAL WEBSITE
 # GET TITLE
-#answer, text = WebAPI.getTitle(protv)
-#print(answer, text)
+# answer, text = WebAPI.getTitle(protv)
+# print(answer, text)
 
 # GET TEXT
-#print(WebAPI.getText(protv))
+# print(WebAPI.getText(protv))
 
 # MessageTwitter
 
@@ -43,9 +42,13 @@ print()
 text = '''
 PLEASE COPY PASTE FROM EXAMPLE.TXT
 '''
+# Text extract
+data = TwitterAPI.getDataFromTwitter(entry)
 
 print("Likes:", TwitterAPI.getLikes(text))
 print("Retweets:", TwitterAPI.getRetweets(text))
 print("Quote Tweets", TwitterAPI.getQuoteTweets(text))
 
-print("Text:", TwitterAPI.getDataFromTwitter(entry))
+print("There are",TextAPI.getWrongWordsNumbers(data),"wrong words out of",TextAPI.getWordsNumber(data))
+
+print("Text:", data)
