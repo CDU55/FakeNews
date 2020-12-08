cdacb4ac326091fda3973a8fbc8c12089a87fe9a
BOBIRE <mraboby@gmail.com>
2020-12-08 10:01:05 +0200

    Example for Twitter Extract side

--- ServerComponent/TextExtraction/main.py
+++ ServerComponent/TextExtraction/main.py
@@ -2,7 +2,7 @@ import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
-from TextExtraction.APIs import WebAPI, TwitterAPI
+from APIs import WebAPI, TwitterAPI
 
 protv = 'https://incont.stirileprotv.ro/joburi-romania/cum-se-va-desfasura-telemunca-si-munca-la-domiciliu-si-cum-se-schimba-programul-de-lucru-noi-reglementari.html?_ga=2.18512444.441552134.1604862638-837112706.1604862638'
 fblink = 'https://www.facebook.com/PresedinteleTraianBasescu/posts/3918068968206625'
@@ -23,8 +23,29 @@ test = "https://mobile.twitter.com/RockstarGames/status/1327306496217128967"
 
 
 # TESTING FOR TWITTER GetData
-#print(TwitterAPI.getDataFromTwitter(test))
-#print(TwitterAPI.getProfileName(test))
-print(TwitterAPI.checkVerifiedAccount(TwitterAPI.getProfileName(test)))
 
-#TwitterAPI.checkVerifiedAccount(TwitterAPI.getProfileName(test))
+entry = twitter
+
+print("Data about user profile")
+print()
+
+user = TwitterAPI.getProfileName(entry)
+print("Username:", user)
+print("Verified:", TwitterAPI.checkVerifiedAccount(user))
+print("Number of followers:", TwitterAPI.getFollowers(user))
+print("Number of tweets:", TwitterAPI.getTweets(user))
+
+print()
+print("Data about post")
+print()
+
+# DELETE THIS // ONLY FOR TEST
+text = '''
+PLEASE COPY PASTE FROM EXAMPLE.TXT
+'''
+
+print("Likes:", TwitterAPI.getLikes(text))
+print("Retweets:", TwitterAPI.getRetweets(text))
+print("Quote Tweets", TwitterAPI.getQuoteTweets(text))
+
+print("Text:", TwitterAPI.getDataFromTwitter(entry))
